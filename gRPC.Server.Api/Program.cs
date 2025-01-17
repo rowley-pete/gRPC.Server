8a6fd8ef6094f817efe0a2c36b9713913ec3e24b
Peter Rowley <rowley.pete@live.co.uk>
2025-01-17 19:24:02 +0000

    Part 2

--- gRPC.Server.Api/Program.cs
+++ gRPC.Server.Api/Program.cs
@@ -6,12 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+  builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
 app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<ApiGrpcService>();
+app.MapGrpcService<ApiGrpcService>().RequireCors("AllowAll").EnableGrpcWeb();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
